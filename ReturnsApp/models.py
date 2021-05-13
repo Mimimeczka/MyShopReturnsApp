5fb56c0c0f68bea21be47d31b1d16323ad9e857a
Mimimeczka <justyna.zaczyk@onet.pl>
2021-05-13 14:48:32 +0200

    create Return model

--- ReturnsApp/models.py
+++ ReturnsApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_id = models.IntegerField()
+    quantity = models.IntegerField()
+
+
+class Return(models.Model):
+    date = models.DateField()
+    products = models.ForeignKey(Product, on_delete=models.CASCADE, default=None, null=True)
+    sum = models.DecimalField(max_digits=10, decimal_places=2)
+    basket_id = models.IntegerField(default=None)
+
+    def __str__(self):
+        return f'Return {self.date}'
