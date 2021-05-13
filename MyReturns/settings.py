912e52829a21b3ce7202c2a996e10aa55784f6f8
Mimimeczka <justyna.zaczyk@onet.pl>
2021-05-13 14:40:54 +0200

    MySQL database connection

--- MyReturns/settings.py
+++ MyReturns/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ReturnsApp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'MyReturns.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ReturnsApp',
+        'USER': 'ReturnsUser',
+        'PASSWORD': 'ReturnsUser',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
